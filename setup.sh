faeb13996018d79b1b289a15a8cbfc29e26e74ce
Edward Li <jiaxi.edwardli@gmail.com>
2024-06-01 01:07:27 -0700

    Fixed setup script

--- setup.sh
+++ setup.sh
@@ -5,13 +5,13 @@ set -e
 SRC_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 BACKUP_DIR="$HOME/.dotfiles-backup/$(date +"%Y-%m-%d-%s")"
 
-if [ $1 != "nobackup" ]; then
+if [ "$1" != "nobackup" ]; then
     echo Original files will be saved in $BACKUP_DIR
     mkdir -p "$BACKUP_DIR"
 fi
 
 ls -A1 "$SRC_DIR/dotfiles" | while read i; do
-    if [ $1 != "nobackup" ]; then
+    if [ "$1" != "nobackup" ]; then
         ! mv "$HOME/$i" "$BACKUP_DIR" 2>/dev/null
     else
         ! rm -rf -- "$HOME/$i"
