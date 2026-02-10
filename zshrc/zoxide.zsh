fea8efeaa6c84ac94110460c4ae8c9979f1c276f
Edward Li <jiaxi.edwardli@gmail.com>
2026-02-09 17:36:51 -0800

    Automatically replace cd with zoxide if its installed

--- zshrc/zoxide.zsh
+++ zshrc/zoxide.zsh
@@ -1,4 +1,8 @@
 # Enable zoxide if installed
 if command -v zoxide >/dev/null 2>&1; then
   eval "$(zoxide init zsh)"
+  # If DISABLE_ZOXIDE_CD is not set, alias cd to zoxide
+  if [[ -z "$DISABLE_ZOXIDE_CD" ]]; then
+    alias cd='z'
+  fi
 fi
