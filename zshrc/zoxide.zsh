2a87b43e2826bbf36b4bad063800cca05fbe9b59
Edward Li <jiaxi.edwardli@gmail.com>
2026-02-17 23:47:38 -0800

    Create better mapping for zoxide cd

--- zshrc/zoxide.zsh
+++ zshrc/zoxide.zsh
@@ -1,8 +1,9 @@
 # Enable zoxide if installed
 if command -v zoxide >/dev/null 2>&1; then
-  eval "$(zoxide init zsh)"
   # If DISABLE_ZOXIDE_CD is not set, alias cd to zoxide
   if [[ -z "$DISABLE_ZOXIDE_CD" ]]; then
-    alias cd='z'
+    eval "$(zoxide init --cmd cd bash)"
+  else
+    eval "$(zoxide init zsh)"
   fi
 fi
