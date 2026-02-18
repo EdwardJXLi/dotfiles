ad67ef3fab7ab2fc1e406b4bdbd3175f8e39cc3d
Edward Li <jiaxi.edwardli@gmail.com>
2026-02-17 23:49:31 -0800

    Change zoxide init for zsh

--- zshrc/zoxide.zsh
+++ zshrc/zoxide.zsh
@@ -2,7 +2,7 @@
 if command -v zoxide >/dev/null 2>&1; then
   # If DISABLE_ZOXIDE_CD is not set, alias cd to zoxide
   if [[ -z "$DISABLE_ZOXIDE_CD" ]]; then
-    eval "$(zoxide init --cmd cd bash)"
+    eval "$(zoxide init --cmd cd zsh)"
   else
     eval "$(zoxide init zsh)"
   fi
